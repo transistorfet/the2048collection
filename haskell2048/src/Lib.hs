103495a2ce5bf70854a99ee6c8ad471196291c55
transistor <trans@jabberwocky.ca>
2022-02-13 20:07:01 -0800

    Updated Haskell version and added notes

--- haskell2048/src/Lib.hs
+++ haskell2048/src/Lib.hs
@@ -3,73 +3,99 @@ module Lib (mainFunc) where
 import Debug.Trace
 
 import System.Random
-import System.IO.Unsafe
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
+
+
+squares = 4
+
+windowSizeX = 800
+windowSizeY = 800
+
+squareSize = (fromIntegral windowSizeX) / (fromIntegral squares) :: Float
+squareSizeInner = squareSize - (squareSize / 10) :: Float
+
 
 data World = World {
   seed :: StdGen,
   rows :: [[Int]]
 }
 
+data Direction =
+    Forward
+  | Reverse
+
+
+-- Main Entry Point
 mainFunc :: IO ()
-mainFunc =
+mainFunc = do
+  gen <- getStdGen
   interactIO
-    (InWindow "Test" (800, 600) (10, 10))
+    (InWindow "2048" (windowSizeX, windowSizeY) (10, 10))
     (greyN 0.5)     -- background color
-    (generate ())   -- initial world
+    (generate gen)  -- initial world
     render          -- function to convert world to a Picture
     input           -- function to handle input events
     modify          -- function to step the world one iteration
 
 
-generate :: () -> World
-generate () =
+-- Generate an empty world
+generate :: StdGen -> World
+generate gen =
   World {
-    seed = mkStdGen 123,
-    rows = [ [ 0, 0, 4, 0]
-           , [ 0, 0, 2, 2]
-           , [ 0, 0, 2, 4]
-           , [ 0, 8, 2, 0] ]
+    seed = gen,
+    rows = map (\_ -> map (\_ -> 0) [0..(squares - 1)]) [0..(squares - 1)]
   }
 
 
+-- Respond to an Up arrow press by shifting all the numbers upward, combining any like numbers as we go
 shiftWorldUp :: World -> World
 shiftWorldUp world =
-  let (rs, newSeed) = randomSequence (seed world) 4 [] in
-  let newRows = toColumns (collapseAndFillRows rs (\(rows, zeros) -> rows ++ zeros) (toColumns (rows world))) in
+  let (rs, newSeed) = randomSequence (seed world) squares [] in
+  let newRows = toColumns (collapseAndFillRows rs Forward (toColumns (rows world))) in
   World { seed = newSeed, rows = newRows }
 
-
+-- Same for Down arrow
 shiftWorldDown :: World -> World
 shiftWorldDown world =
-  let (rs, newSeed) = randomSequence (seed world) 4 [] in
-  let newRows = toColumns (collapseAndFillRows rs (\(rows, zeros) -> reverse zeros ++ rows) (toColumns (rows world))) in
+  let (rs, newSeed) = randomSequence (seed world) squares [] in
+  let newRows = toColumns (collapseAndFillRows rs Reverse (toColumns (rows world))) in
   World { seed = newSeed, rows = newRows }
 
-
+-- Same for Left arrow
 shiftWorldLeft :: World -> World
 shiftWorldLeft world =
-  let (rs, newSeed) = randomSequence (seed world) 4 [] in
-  let newRows = collapseAndFillRows rs (\(rows, zeros) -> rows ++ zeros) (rows world) in
+  let (rs, newSeed) = randomSequence (seed world) squares [] in
+  let newRows = collapseAndFillRows rs Forward (rows world) in
   World { seed = newSeed, rows = newRows }
 
-
+-- Same for Right arrow
 shiftWorldRight :: World -> World
 shiftWorldRight world =
-  let (rs, newSeed) = randomSequence (seed world) 4 [] in
-  let newRows = collapseAndFillRows rs (\(rows, zeros) -> reverse zeros ++ rows) (rows world) in
+  let (rs, newSeed) = randomSequence (seed world) squares [] in
+  let newRows = collapseAndFillRows rs Reverse (rows world) in
   World { seed = newSeed, rows = newRows }
 
 
-collapseAndFillRows :: [Double] -> (([Int], [Int]) -> [Int]) -> [[Int]] -> [[Int]]
-collapseAndFillRows rs f rows =
-  map (\(i, row) ->
-    let newRow = collapseNumbers row
-    in f (newRow, (fillZeros (rs !! i) (4 - (length newRow)))))
-  (zip [0..] rows)
+-- Collapse the rows left or right depending on the direction, filling in with zeros and a random
+-- chance that a new number will appear on the opposite side of the direction
+collapseAndFillRows :: [Double] -> Direction -> [[Int]] -> [[Int]]
+collapseAndFillRows rs dir rows =
+  map forEachRow (zip [0..] rows)
+  where
+    forEachRow (i, row) =
+      let newRow = maybeReverse dir $ collapseNumbers $ maybeReverse dir row in
+      let zeros = fillZeros (rs !! i) (squares - (length newRow)) in
+      case dir of
+        Forward -> newRow ++ zeros
+        Reverse -> (reverse zeros) ++ newRow
 
+-- Return either a forward or reversed list based on the direction value
+maybeReverse :: Direction -> [Int] -> [Int]
+maybeReverse Forward list = list
+maybeReverse Reverse list = reverse list
 
+-- Remove all the zeros from a list, and add any adjacent numbers that have the same value
 collapseNumbers :: [Int] -> [Int]
 collapseNumbers []            = []
 collapseNumbers (0 : remain)  = collapseNumbers remain
@@ -82,13 +108,13 @@ collapseNumbers (first : second : remain) =
   else
     first : collapseNumbers (second : remain)
 
-
+-- Return a list of zeros of the given size, but the last zero has a random chance of being a 2 instead
 fillZeros :: Double -> Int -> [Int]
 fillZeros r 0   = []
-fillZeros r 1   = [ if r < 0.1 then 2 else 0 ]
+fillZeros r 1   = [ if r < 0.2 then 2 else 0 ]
 fillZeros r len = 0 : fillZeros r (len - 1)
 
-
+-- Return a list of random numbers of the given length, along with an updated random seed
 randomSequence :: StdGen -> Int -> [Double] -> ([Double], StdGen)
 randomSequence g 0 list =
   (list, g)
@@ -96,40 +122,56 @@ randomSequence g n list =
   let (r, nextg) = randomR (0.0, 1.0) g
   in randomSequence nextg (n - 1) (r : list)
 
-
+-- Transpose the rows with the columns in a matrix
 toColumns :: [[Int]] -> [[Int]]
 toColumns rows =
   map (\i ->
     map (\row -> (row !! i)) rows)
-    [0..3]
+    [0..(squares - 1)]
 
 
+-- Draw the world
 render :: World -> IO Picture
 render world =
   return $ pictures $ renderAllRows (rows world)
 
-
+-- Draw all the rows
 renderAllRows :: [[Int]] -> [Picture]
 renderAllRows rows =
   map forEachRow (zip [0..] rows)
   where
     forEachRow (i, entry) =
-      pictures $ renderRow (200 + (i * (-200))) entry
+      let y = offsetOf i in
+      pictures $ renderRow (-1 * y) entry
 
-
+-- Draw a single row
 renderRow :: Float -> [Int] -> [Picture]
 renderRow y row =
   map forEachTile (zip [0..] row)
   where
     forEachTile (i, entry) =
-      Translate (-200 + (i * (200))) y $ renderTile entry
+      let x = offsetOf i in
+      Translate x y $ renderTile entry
 
+-- Get the offset from the centre of the screen for a square on a given row or column
+offsetOf :: Float -> Float
+offsetOf i =
+  ((i - (fromIntegral squares / 2)) * squareSize + (squareSize / 2))
 
+-- Draw a single square with its number
 renderTile :: Int -> Picture
 renderTile number =
-  pictures [ color (greyN 0.1) $ rectangleSolid 180 180, color white $ Translate (-180 / 4) (-180 / 4) $ text (show number) ]
+  let len = fromIntegral $ length (show number) in
+  pictures [
+    color (greyN 0.1) $
+      rectangleSolid squareSizeInner squareSizeInner
 
+  , color white $
+      Translate (-squareSizeInner / 8) (-squareSizeInner / 8) $ Scale (0.5 / len) (0.5 / len) $
+        text (show number)
+  ]
 
+-- Handle input events and call the appropriate shift function
 input :: Event -> World -> IO World
 input event world =
   case event of
@@ -142,8 +184,8 @@ input event world =
         _ -> world
     _ -> return world
 
-
+-- Not used, called to update after input?
 modify :: Controller -> IO ()
 modify controller =
-  putStrLn "thing"
+  return ()
 
